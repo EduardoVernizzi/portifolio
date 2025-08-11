5307028a4c5453d4bd538892c4e418fa8f52bb9f
Eduardo Vernizzi <eduardo.vernizzi@hotmail.com>
2025-08-11 14:58:45 -0300

    Update: Adicionando stacks na seção Skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 import htmlLogo from '../assets/html.png';
 import cssLogo from '../assets/css.png';
 import jsLogo from '../assets/js.png';
@@ -6,11 +8,13 @@ import bootstrapLogo from '../assets/bootstrap.png';
 import gitLogo from '../assets/git.png';
 import mySqlLogo from '../assets/mysql.png';
 import nodeLogo from '../assets/node.png';
-
-
-
+import csharp from '../assets/csharp.png';
+import sassLogo from '../assets/sass.png';
+import fireLogo from '../assets/firebase.png';
+import expressLogo from '../assets/express.png'
 
 const skills = [
+  // Front-end
   {
     name: 'HTML5',
     image: htmlLogo,
@@ -22,6 +26,16 @@ const skills = [
     description: 'Estilização responsiva com foco em usabilidade.'
   },
   {
+    name: 'Sass',
+    image: sassLogo,
+    description: 'Pré-processador CSS que facilita a criação de estilos modulares, reutilizáveis e organizados, com recursos como variáveis, mixins e aninhamento.'
+  },
+  {
+    name: 'Bootstrap',
+    image: bootstrapLogo,
+    description: 'Layouts rápidos e responsivos com grid e utilitários.'
+  },
+  {
     name: 'JavaScript',
     image: jsLogo,
     description: 'Lógica dinâmica e interatividade no front-end.'
@@ -31,45 +45,57 @@ const skills = [
     image: reactLogo,
     description: 'Componentização e construção de interfaces performáticas.'
   },
+
+  // Back-end
   {
-    name: 'Bootstrap',
-    image: bootstrapLogo,
-    description: 'Layouts rápidos e responsivos com grid e utilitários.'
+    name: 'Node',
+    image: nodeLogo,
+    description: 'Execução de código JavaScript no back-end com foco em performance e escalabilidade.'
   },
   {
-    name: 'Git',
-    image: gitLogo,
-    description: 'Controle de versão com Git e GitHub.'
+    name: 'Express',
+    image: expressLogo,
+    description: 'Framework minimalista para Node.js que facilita a criação de APIs e aplicações web robustas, com roteamento simples e suporte a middlewares.'
   },
+  {
+    name: 'C#',
+    image: csharp,
+    description: 'Desenvolvimento de aplicações robustas e escaláveis com foco em back-end, utilizando conceitos de orientação a objetos.'
+  },
+
+  // Banco de dados
   {
     name: 'MySQL',
     image: mySqlLogo,
     description: 'Gerenciamento de dados relacionais com consultas eficientes em SQL.'
   },
   {
-    name: 'Node',
-    image: nodeLogo,
-    description: 'Execução de código JavaScript no back-end com foco em performance e escalabilidade.'
+    name: 'Firebase',
+    image: fireLogo,
+    description: 'Plataforma da Google para desenvolvimento de aplicações web e mobile, oferecendo serviços como autenticação, banco de dados em tempo real, armazenamento, hosting e funções serverless.'
   },
+
+  // Auxiliares
+  {
+    name: 'Git',
+    image: gitLogo,
+    description: 'Controle de versionamento de código.'
+  }
 ];
 
-function Skills() {
+export default function Skills() {
   return (
     <section className="skills-section" id="skills">
-      <div className="skills-container">
-        <h2 data-aos="fade-up">Habilidades</h2>
-        <div className="skills-grid">
-          {skills.map((skill, index) => (
-            <div className="skill-card" data-aos="fade-up" key={index}>
-            <img src={skill.image} alt={skill.name} className="skill-img" />
-              <h3>{skill.name}</h3>
-              <p>{skill.description}</p>
-            </div>
-          ))}
-        </div>
+      <h2>Habilidades</h2>
+      <div className="skills-grid">
+        {skills.map(({ name, image, description }, index) => (
+          <div className="skill-card" key={index} style={{ animationDelay: `${index * 0.15}s` }}>
+            <img src={image} alt={name} />
+            <h3>{name}</h3>
+            <p>{description}</p>
+          </div>
+        ))}
       </div>
     </section>
   );
 }
-
-export default Skills;
